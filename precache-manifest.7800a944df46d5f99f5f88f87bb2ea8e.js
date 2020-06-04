84fe7060bfb211840532f012aa992865fb843069
SergioBurlaka <burlaka01@gmail.com>
2020-06-04 11:59:35 +0300

    Updates

---
+++ precache-manifest.7800a944df46d5f99f5f88f87bb2ea8e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c98c9544680127a21743bc51b9014a8a",
+    "url": "/Reviewers-page/index.html"
+  },
+  {
+    "revision": "e59576654488fa343656",
+    "url": "/Reviewers-page/static/css/main.894e728c.chunk.css"
+  },
+  {
+    "revision": "32af0f0bc695efb006c4",
+    "url": "/Reviewers-page/static/js/2.4facbdb3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Reviewers-page/static/js/2.4facbdb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e59576654488fa343656",
+    "url": "/Reviewers-page/static/js/main.c23894de.chunk.js"
+  },
+  {
+    "revision": "b0b299e7a23a8c3bbb8b",
+    "url": "/Reviewers-page/static/js/runtime-main.694f6189.js"
+  },
+  {
+    "revision": "b23dab13653015a17eff4993b7efcfd4",
+    "url": "/Reviewers-page/static/media/Beth.b23dab13.jpg"
+  },
+  {
+    "revision": "b981199b8182db4bdde83c9396357c6e",
+    "url": "/Reviewers-page/static/media/Jerry.b981199b.jpg"
+  },
+  {
+    "revision": "6cf19b5ec3315c1a3fafb7278ea975ed",
+    "url": "/Reviewers-page/static/media/MeeseeksHQ.6cf19b5e.png"
+  },
+  {
+    "revision": "46484a0023e7ce1abc3b776bf441caab",
+    "url": "/Reviewers-page/static/media/Morty.46484a00.jpg"
+  },
+  {
+    "revision": "2ddfabba40860debf2e1faa80969439f",
+    "url": "/Reviewers-page/static/media/Rick.2ddfabba.jpg"
+  },
+  {
+    "revision": "d5afbba3c1664e2bc5b328a09b8beaed",
+    "url": "/Reviewers-page/static/media/Summer.d5afbba3.jpg"
+  }
+]);
